0fcb2d7e9b573f25e29fa55b0aba6e52665a80f5
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-05 22:31:11 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace asciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(" \\\\()//");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+        }
+    }
+}
